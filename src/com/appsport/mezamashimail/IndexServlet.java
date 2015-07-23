297955808a443ffac1f6e5d29eaccf959982d067
esmilehm <e.smile.hm@gmail.com>
2015-07-23 20:07:12 +0900

    コメント修正
    GitHub側から修正

--- src/com/appsport/mezamashimail/IndexServlet.java
+++ src/com/appsport/mezamashimail/IndexServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 // TODO 20150721より、次回はP104の動作確認以降からサンプルファイルをコピーしていく。
-// TODO 20150724より、コメント追加
+// TODO 20150724より、コメント追加２
 /**
  * 依頼登録サーブレット
  * @author
